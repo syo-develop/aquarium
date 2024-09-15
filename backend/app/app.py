b9cb5bd3efdf52257cf8b7ebcda74d624126ec3f
shota-kito <shota-kito@azapa-eng.co.jp>
2024-09-16 00:20:51 +0900

    supports_credentials=True

--- backend/app/app.py
+++ backend/app/app.py
@@ -5,7 +5,7 @@ from waitress import serve
 
 app = Flask(__name__, static_folder="./build/static", template_folder="./build")
 app.register_blueprint(main)
-CORS(app)
+CORS(app, supports_credentials=True)
 
 if __name__ == '__main__':
      # app.run(debug=True)
