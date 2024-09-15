62e30d28cfe2ce43350560647704835e27c3100f
shota-kito <shota-kito@azapa-eng.co.jp>
2024-09-15 22:49:15 +0900

    waitress追加

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify
 from controllers.main_controller import main
 from flask_cors import CORS
+from waitress import serve
 
 app = Flask(__name__, static_folder="./build/static", template_folder="./build")
 app.register_blueprint(main)
 CORS(app)
 
 if __name__ == '__main__':
-     app.run(debug=True)
+     # app.run(debug=True)
+     serve(app, host='0.0.0.0', port=5000)
